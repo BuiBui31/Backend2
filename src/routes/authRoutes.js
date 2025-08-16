0f128df11f3715b2963a8c976ac752197e1659f3
bluis <luis.bapst@gmx.ch>
2025-08-16 12:21:59 +0200

    Adding Primsa

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,60 +1,61 @@
-import express from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import db from "../db.js";
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import db from '../db.js';
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-  const { username, password } = req.body;
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
 
-  const hashedPassword = bcrypt.hashSync(password, 8);
+    const hashedPassword = bcrypt.hashSync(password, 8);
 
-  try {
-    const insertUser = db.prepare(
-      `INSERT INTO users (username, password) VALUES (?,?)`
-    );
-    const result = insertUser.run(username, hashedPassword);
+    try {
+        const user = await prisma.user.create({
+            data: {
+                username,
+                password: hashedPassword,
+            },
+        });
+        const defualtTodo = `Hello :D Add you first todo!`;
+        await prisma.todo.create({
+            data: {
+                task: defualtTodo,
+                userId: user.id,
+            },
+        });
 
-    const defualtTodo = `Hello :D Add you first todo!`;
-    const insertTodo = db.prepare(
-      `INSERT INTO todos (user_id, task) VALUES (?, ?)`
-    );
-    insertTodo.run(result.lastInsertRowid, defualtTodo);
-
-    const token = jwt.sign(
-      { id: result.lastInsertRowid },
-      process.env.JWT_SECRET,
-      { expiresIn: "24h" }
-    );
-    res.json({ token });
-  } catch (error) {
-    console.log(error.message);
-    res.sendStatus(503);
-  }
+        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, {
+            expiresIn: '24h',
+        });
+        res.json({ token });
+    } catch (error) {
+        console.log(error.message);
+        res.sendStatus(503);
+    }
 });
 
-router.post("/login", (req, res) => {
-  const { username, password } = req.body;
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
 
-  try {
-    const getUser = db.prepare("SELECT * FROM users WHERE username = ?");
-    const user = getUser.get(username);
-    if (!user) {
-      return res.status(404).send({ message: "user not found" });
+    try {
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?');
+        const user = getUser.get(username);
+        if (!user) {
+            return res.status(404).send({ message: 'user not found' });
+        }
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+        if (!passwordIsValid) {
+            return res.status(401).send({ message: 'Invalid Password' });
+        }
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+            expiresIn: '24h',
+        });
+        res.json({ token });
+    } catch (error) {
+        console.log(error.message);
+        res.sendStatus(503);
     }
-    const passwordIsValid = bcrypt.compareSync(password, user.password);
-    if (!passwordIsValid) {
-      return res.status(401).send({ message: "Invalid Password" });
-    }
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "24h",
-    });
-    res.json({ token });
-  } catch (error) {
-    console.log(error.message);
-    res.sendStatus(503);
-  }
 });
 
 export default router;
